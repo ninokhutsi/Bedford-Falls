11fa7cbd94964f3941caf02a683c2a8fbae2d274
ninokhutsi <nino.khutsiberidze12b@geolab.edu.ge>
2020-11-02 12:15:15 +0400

    add navigation
    @media(min-widt:1024,768px)

---
+++ app.js
@@ -0,0 +1,49 @@
+$(document).ready(function(){ 
+    
+    $("#burger").on("click", function(e){
+        // console.log(e); // all events
+       
+        if($(this).hasClass("active")){
+            $(this).removeClass("active");
+        }else{
+            $(this).addClass("active");
+        };
+        if($("#navigation").hasClass("active")){
+            $("#navigation").removeClass("active");
+        }else{
+            $("#navigation").addClass("active");
+        };
+
+        
+
+    });
+    
+   
+
+//     var slideIndex = 1;
+// showSlides(slideIndex);
+
+// function plusSlides(n) {
+//   showSlides(slideIndex += n);
+// }
+
+// function currentSlide(n) {
+//   showSlides(slideIndex = n);
+// }
+
+// function showSlides(n) {
+//   var i;
+//   var slides = document.getElementsByClassName("mySlides");
+//   var dots = document.getElementsByClassName("dot");
+//   if (n > slides.length) {slideIndex = 1}
+//     if (n < 1) {slideIndex = slides.length}
+//     for (i = 0; i < slides.length; i++) {
+//       slides[i].style.display = "none";
+//     }
+//     for (i = 0; i < dots.length; i++) {
+//       dots[i].className = dots[i].className.replace(" active", "");
+//     }
+//   slides[slideIndex-1].style.display = "block";
+//   dots[slideIndex-1].className += " active";
+// }
+});
